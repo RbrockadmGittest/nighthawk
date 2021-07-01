92e2bf8186ddad1395f2b5257d94ea26f044c98e
Siraaj Ahmed <siraaj.ahmed786@gmail.com>
2021-07-01 09:56:40 +0100

    Setup vue app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <HomeComponent msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HomeComponent from "./components/home-component.vue";
+@Options({
+  components: {
+    HomeComponent,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
